37cc0bb78794a14033f8ad1e92f956ed1b2c709d
Sammy392 <theegreatsammy@gmail.com>
2024-09-19 13:53:55 +0300

    Update Answer.js

--- commandes/Answer.js
+++ commandes/Answer.js
@@ -6,7 +6,7 @@ const { default: axios } = require('axios');
 
 
 
-zokou({nomCom:"popkid",reaction:"📡",categorie:"IA"},async(dest,zk,commandeOptions)=>{
+zokou({nomCom:"sammy",reaction:"📡",categorie:"IA"},async(dest,zk,commandeOptions)=>{
 
   const {repondre,ms,arg}=commandeOptions;
   
