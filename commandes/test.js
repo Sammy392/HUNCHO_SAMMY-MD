345413c771a0aa4f6951b9c902713e7a549ccebc
Sammy392 <theegreatsammy@gmail.com>
2024-09-19 14:55:11 +0300

    Update test.js

--- commandes/test.js
+++ commandes/test.js
@@ -3,8 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 zokou({ nomCom: "test", reaction: "🏎️", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = '𝐇𝐞𝐲 𝐓𝐡𝐢𝐬 𝐢𝐬 *POPKID 𝐌𝐝* \n ' + "𝐓𝐡𝐞 𝐅𝐚𝐬𝐭𝐞𝐬𝐭 𝐖𝐚𝐁𝐨𝐭";
-    let d = '> 𝑩𝒚 *popkid*'
+    let z = '𝐇𝐞𝐲 𝐓𝐡𝐢𝐬 𝐢𝐬 *SAMMY 𝐌𝐝* \n ' + "𝐓𝐡𝐞 𝐅𝐚𝐬𝐭𝐞𝐬𝐭 𝐖𝐚𝐁𝐨𝐭";
+    let d = '> 𝑩𝒚 *SAMMY*'
     let varmess = z + d;
     var img = 'https://i.ibb.co/BNvstq2/IMG-20240916-WA0016.jpg';
     await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
