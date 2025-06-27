ae9fcf2d99efb6ed89b418d1f46bf1cee24a6228
Sammy392 <theegreatsammy@gmail.com>
2025-06-27 20:28:43 +0300

    Update zokou.js

--- framework/zokou.js
+++ framework/zokou.js
@@ -6,7 +6,7 @@ function zokou(obj, fonctions) {
         infoComs.categorie = "General";
     }
     if (!obj.reaction) {
-        infoComs.reaction = "🚔";
+        infoComs.reaction = "✅";
     }
     infoComs.fonction = fonctions;
     cm.push(infoComs);
