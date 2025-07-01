7f155bae75538370c781f7c140988274b69e73cd
popkid <talkdroveke@gmail.com>
2025-07-01 06:46:25 +0300

    Popkid

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import {
     useMultiFileAuthState,
 } from '@whiskeysockets/baileys';
 
-import { Handler, Callupdate, GroupUpdate } from './scs/nitrox/index.js';
+import { Handler, Callupdate, GroupUpdate } from './src/nitrox/index.js';
 import express from 'express';
 import pino from 'pino';
 import fs from 'fs';
