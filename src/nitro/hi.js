b45edad22640212bd1142c648b95f3b1795908c5
popkid🚬🧸 <kenyanpopkid@gmail.com>
2025-09-12 14:14:35 +0300

    Delete src directory

--- src/nitro/hi.js
+++
@@ -1,9 +0,0 @@
-export default async function (m, sock) {
-  const text = m.body?.toLowerCase();
-
-  if (text === ".hi") {
-    await sock.sendMessage(m.chat, {
-      text: "Hello! I'm Huncho Bot 🔥"
-    });
-  }
-}
